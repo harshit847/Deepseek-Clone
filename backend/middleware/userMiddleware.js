137bd4426b7b3455973c1a15f0f0aca27499cd74
harshit847 <harshitrai331@gmail.com>
2025-08-09 01:02:18 +0530

    Push

--- backend/middleware/userMiddleware.js
+++ backend/middleware/userMiddleware.js
@@ -1,19 +1,24 @@
 import jwt from "jsonwebtoken";
 import config from "../config.js";
-function userMiddleware(req, res, next) {
-  const authHeader = req.headers.authorization;
 
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    return res.status(401).json({ errors: "No token provided" });
+function userMiddleware(req, res, next) {
+  let token;
+
+  if (req.headers.authorization?.startsWith("Bearer ")) {
+    token = req.headers.authorization.split(" ")[1];
+  }
+  else if (req.cookies?.jwt) {
+    token = req.cookies.jwt;
   }
 
-  const token = authHeader.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ errors: "No token provided" });
+  }
 
   try {
     const decoded = jwt.verify(token, config.JWT_USER_PASSWORD);
     req.userId = decoded.id;
     next();
-
   } catch (error) {
     console.log("❌ JWT Error:", error);
     return res.status(401).json({ errors: "Invalid token or expired" });
